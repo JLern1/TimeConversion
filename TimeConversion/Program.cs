6723cd09eacde5ed14ff82fedb14e8084fa30d8d
Jason <Jason@PC>
2019-09-27 18:05:21 -0400

    Add project files.

---
+++ TimeConversion/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace TimeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "07:05:45PM";
+            Debug.Print(timeConversion(s));
+        }
+        static string timeConversion(string s)
+        {
+            int hr = Convert.ToInt32(s.Substring(0, 2));
+            string timeWithoutHr = s.Substring(2, s.Length - 4);
+            string newTime;
+            if (hr == 12 && s.EndsWith("AM"))
+            {
+                hr = 0;
+            }
+            else if (hr == 12 && s.EndsWith("PM"))
+            {
+                hr = 12;
+            }
+            else if (s.EndsWith("PM"))
+            {
+                hr += 12;
+            }
+
+            if (hr < 10)
+            {
+                newTime = "0" + hr + timeWithoutHr;
+            }
+            else
+            {
+                newTime = hr + timeWithoutHr;
+            }
+            return newTime;
+        }
+    }
+}
